abe2b4b420f6d706d7aff0f791d58dbfbebb6876
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-25 10:55:02 -0400

    criando a  rota populate

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: (ctx) => {
+    console.log("ok");
+    ctx.send({ ok: true });
+  },
+};
